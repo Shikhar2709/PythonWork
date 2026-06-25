17abee8cb00011eb537f2e466f240fa857eed6bd
Shikhar2709 <srivastava.shikhar2709@gmail.com>
2026-06-25 10:24:15 +0530

    Done Python basic

---
+++ Day1/hello.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+message="Hello World"
+print(message)
+
+print("Hello" + " " + "World")
+
+
+name ="World "
+print (f"Hello {name} ")
+
+def greet ():
+    print("Hello World")
+
+greet()
